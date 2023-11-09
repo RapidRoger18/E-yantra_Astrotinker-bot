87bf82c713dfc0b51443b4b2bd7e970f5c00eb82
RapidRoger18 <Atharvakashalkar1804@gmail.com>
2023-11-09 15:33:58 +0530

    Added ALU for riscV

--- t2b_riscv_cpu/verilog_codes/alu.v
+++ t2b_riscv_cpu/verilog_codes/alu.v
@@ -8,6 +8,7 @@ input [36:0] instructions,
 
 output reg [31:0] ALUoutput
 );
+reg [31:0] Simm;
 
 	initial begin
 		ALUoutput = 0;
@@ -16,27 +17,27 @@ output reg [31:0] ALUoutput
 	always@(*) begin
 	
     case(instructions) 
-		   37'h1 : ALUoutput <= rs1 + rs2;                                       //add
-           37'h2 : ALUoutput <= rs1 - rs2;                                       //sub
-           37'h4 : ALUoutput <= rs1 ^ rs2;                                       //xor
-           37'h8 : ALUoutput <= rs1 | rs2;                                       //or
-           37'h10 : ALUoutput <= rs1 & rs2;                                      //and
-           37'h20 : ALUoutput <= rs1 << rs2;                                     //sll
-           37'h40 : ALUoutput <= rs1 >> rs2;                                     //srl
-           37'h80 : ALUoutput <= rs1 > rs2;                                      //sra 
-           37'h100 : ALUoutput <= (rs1 > rs2)?1:0;                               //slt
-           37'h200 : ALUoutput <= (rs1 > rs2)?1:0;                               //sltu
-           37'h400 : ALUoutput <= (rs1 + imm);                                   //addi
-           37'h800 : ALUoutput <= (rs1 ^ imm);                                   //xori
-           37'h1000 : ALUoutput <= (rs1 | imm);                                  //ori
-           37'h2000 : ALUoutput <= (rs1 & imm);                                  //andi
-           37'h4000 : ALUoutput <= (rs1 << imm[4:0]);                            //slli
-           37'h8000 : ALUoutput <= (rs1 >> imm[4:0]);                            //srli
-           37'h10000 : ALUoutput <= (rs1 > imm[4:0]);                            //srai
-           37'h20000 : ALUoutput <= (rs1 < imm)?1:0;                             //slti
-           37'h40000 : ALUoutput <= (rs1 < imm)?1:0;                             //sltiu 
+		   37'h1 : ALUoutput <= rs1 + rs2;                                         //add
+         37'h2 : ALUoutput <= rs1 - rs2;                                       //sub
+         37'h4 : ALUoutput <= rs1 ^ rs2;                                       //xor
+         37'h8 : ALUoutput <= rs1 | rs2;                                       //or
+         37'h10 : ALUoutput <= rs1 & rs2;                                      //and
+         37'h20 : ALUoutput <= rs1 << rs2[4:0];                                //sll
+         37'h40 : ALUoutput <= rs1 >> rs2[4:0];                                //srl
+         37'h80 : ALUoutput <= rs1 >>> rs2[4:0];                               //sra 
+         37'h100 : ALUoutput <= (rs1 < rs2);                                   //slt
+         37'h200 : ALUoutput <= (rs1 < rs2);                                   //sltu
+         37'h400 : ALUoutput <= (rs1 + Simm);                                  //addi
+         37'h800 : ALUoutput <= (rs1 ^ Simm);                                  //xori
+         37'h1000 : ALUoutput <= (rs1 | Simm);                                 //ori
+         37'h2000 : ALUoutput <= (rs1 & Simm);                                 //andi
+         37'h4000 : ALUoutput <= (rs1 << imm[4:0]);                            //slli
+         37'h8000 : ALUoutput <= (rs1 >> imm[4:0]);                            //srli
+         37'h10000 : ALUoutput <= (rs1 >>> imm[4:0]);                          //srai
+         37'h20000 : ALUoutput <= (rs1 < Simm);                                //slti
+         37'h40000 : ALUoutput <= (rs1 < Simm);                                //sltiu 
           
-           default : ALUoutput<= 0;
+         default : ALUoutput<= 0;
         
 		
 	endcase  
