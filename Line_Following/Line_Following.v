75fecd62a4495e0e5ce9566b6df939cedcd040cb
Atharv1035 <atharv.sp1005@gmail.com>
2024-01-02 20:20:29 +0530

    ADDED ALL CODES WITH LATEST LINE FOLLOWING CODE

--- Line_Following/Line_Following.v
+++ Line_Following/Line_Following.v
@@ -21,7 +21,7 @@ module Line_Following(
 		
 reg [3:0]node=0;
 parameter Kp = 1;
-parameter Kd = 1;
+parameter Kd = 2;
 parameter Ki = 1;
 
 reg read_left, read_center,read_right ;
@@ -38,46 +38,47 @@ always @(*) begin
 		//{led1_R1,led2_R2,led3_R3} <= 2'b0;
 	   //{led1_B1,led2_B2,led3_B3} <= 3'b0;
 		//{led1_G1,led2_G2,led3_G3} <= 3'b111;
-		dutycyc_left<=4'd8;
-		dutycyc_right<=4'd8;
+//		dutycyc_left<=4'd8;
+//		dutycyc_right<=4'd8;
 	if (left>12'd1200 && middle>12'd1200 && right>12'd1200 && !node_flag) begin
 					m1_a<=1;
 					m1_b<=0;
 					m2_a<=0;
 					m2_b<=1;
-					dutycyc_left<=dutycyc_left+2;
-					dutycyc_right<=dutycyc_right-5;
-					//{led1_R1,led2_R2,led3_R3} <= 2'b0;
-				   //{led1_B1,led2_B2,led3_B3} <= 3'b111;
-					//{led1_G1,led2_G2,led3_G3} <= 3'b0;
+					dutycyc_left<=4'd10;
+					dutycyc_right<=4'd2;
+//					//{led1_R1,led2_R2,led3_R3} <= 2'b0;
+//				   //{led1_B1,led2_B2,led3_B3} <= 3'b111;
+//					//{led1_G1,led2_G2,led3_G3} <= 3'b0;
 					node <= node + 1;
 					node_flag <= 1;  
+	end
+	else if ( left > 1200 && right < 700) begin
+						m1_a<=0; // *
+						m1_b<=1;
+						m2_a<=1;
+						m2_b<=0;
+						dutycyc_left<=4'd3;
+						dutycyc_right<=4'd10;
+						node_flag <= 0;
 	end
 	else if ( right > 1200 && left < 700) begin
 						m1_a<=1;
 						m1_b<=0;
-						m2_a<=1;	
-						m2_b<=0;
-						dutycyc_left<=4'd10;
-						dutycyc_right<=4'd2;
+						m2_a<=0;	
+						m2_b<=1;
+						dutycyc_left<=4'd9;
+						dutycyc_right<=4'd3;
 						node_flag <= 0;
 	end
-	else if ( left > 1300 && right < 700) begin
-						m1_a<=1; // *
-						m1_b<=0;
-						m2_a<=1;
-						m2_b<=0;
-						dutycyc_left<=dutycyc_left-5;
-						dutycyc_right<=dutycyc_right+2;
-						node_flag <= 0;
-	end
+	
 	else if (left < 700 && middle > 1200  && right < 700 ) begin
 						m1_a<=1;
 						m1_b<=0;
 						m2_a<=1;
 						m2_b<=0;
-						dutycyc_left<=4'd8;
-						dutycyc_right<=4'd8;
+						dutycyc_left<=4'd7;
+						dutycyc_right<=4'd7;
 						node_flag <= 0;
 	end					
 	else if (left<700 && middle < 700 && right < 700 )begin
@@ -85,15 +86,15 @@ always @(*) begin
 						m1_b<=0;
 						m2_a<=0;	
 						m2_b<=1;
-						dutycyc_left<=dutycyc_left+1;
-						dutycyc_right<=dutycyc_right-4;
+						dutycyc_left<=4'd8;
+						dutycyc_right<=4'd2;
 	end
 
 //	if(!node_flag) begin
 		 if (left<700) read_left<=0;
 		 else read_left<=1;
 
-		 if (middle>1300) read_center<=1;
+		 if (middle>1200) read_center<=1;
 		 else read_center<=0;
 
 		  if (right<700) read_right<=0;
